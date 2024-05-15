4a5309c36ebb08ba09bd076195e09ba7dc5b590d
Alisson Castro <alissoncastroskt@gmail.com>
2024-05-15 18:35:39 -0300

    ajustes path root
    ajustando path root e iniciando adapter com o serviço car resale

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from src.adapters.driving.rest.v1.login import router as login_router
 from src.adapters.driving.rest.v1.settings import router as settings_router
 from src.configuration.dependency_injection import Container
 
-
+PATH_ROOT = "/fiap-car-resale/admin"
 container = Container()
 
 app = FastAPI(
-    title="Resale Car API",
+    title="Resale Car API ADMIN",
     description="API for managing cars for resale",
     version="1.0.0",
-    openapi_url="/api/v1/openapi.json",
-    docs_url="/api/v1/docs",
+    openapi_url=f"{PATH_ROOT}/api/v1/openapi.json",
+    docs_url=f"{PATH_ROOT}/api/v1/docs",
     license_info={
         "name": "MIT",
         "url": "https://opensource.org/licenses/MIT",
@@ -22,12 +22,11 @@ app = FastAPI(
 )
 
 
-
-@app.get("/api/v1/health")
+@app.get(f"{PATH_ROOT}/api/v1/health")
 async def health():
     return {"status": "ok"}
 
-app.include_router(login_router, tags=["login"])
-app.include_router(users_router, tags=["users"])
-app.include_router(cars_router, tags=["cars"])
-app.include_router(settings_router, tags=["settings"])
+app.include_router(login_router, tags=["login"], prefix=f"{PATH_ROOT}")
+app.include_router(users_router, tags=["users"], prefix=f"{PATH_ROOT}")
+app.include_router(cars_router, tags=["cars"], prefix=f"{PATH_ROOT}")
+app.include_router(settings_router, tags=["settings"], prefix=f"{PATH_ROOT}")
